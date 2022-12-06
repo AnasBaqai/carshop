dbc994baac2de5582d12e4d118ea3137d66bde7c
Anas Baqai <anashabib139@gmail.com>
2022-12-07 02:08:17 +0500

    db pass fix

--- database.js
+++ database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'node418489-onlineshopping.j.layershift.co.uk',
     user: 'root',
-    password: 'fast',
-    database: 'BPHpoy47384'
+    password: 'BPHpoy47384',
+    database: 'carshop'
 });
 
 module.exports = db;
